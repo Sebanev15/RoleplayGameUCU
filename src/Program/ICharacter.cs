371f8b90f5ef22e6709a1b35abc3337ef56c4ddc
Sebastián Neves <sebastian.neves@correo.ucu.edu.uy>
2025-09-20 10:26:27 -0300

    Implement the character interface with his respective attributes and methods

---
+++ src/Program/ICharacter.cs
@@ -0,0 +1,22 @@
+﻿namespace Program;
+
+public interface ICharacter
+{
+    public string Name { get; set; }
+    public float Health { get; set; }
+    public List<IItem> Items { get; }
+    public int Defense { get; set; }
+    public int Damage { get; set; }
+
+    public void Attack(ICharacter characterAttacked);
+
+    public void Heal(ICharacter characterHealed);
+
+    public void AddItem(IItem itemAdded);
+
+    public void RemoveItem(IItem itemRemoved);
+
+    public int GetTotalDamage();
+
+    public int GetTotalDefense();
+}
