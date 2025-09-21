1b9db9b5f91486e8dcb6fd7505ae0e72db42986e
Hernan Lopez <hlhernano@gmail.com>
2025-09-21 19:32:45 -0300

    Creo instancias y uso metodos en el program

--- src/Program/Program.cs
+++ src/Program/Program.cs
@@ -4,6 +4,15 @@ class Program
 {
     static void Main(string[] args)
     {
+        Dwarf enano = new Dwarf("Karl", 100, 45, 50);
+
+        Healing potion = new Healing("Pocion Sanadora", 0, true, 30);
+        Weapon sword = new Weapon("Espada", 30, 5, false);
         
+        enano.AddItem(potion);
+        enano.RemoveItem(sword);
+        enano.AddItem(sword);
+        enano.RemoveItem(potion);
+        enano.AddItem(sword);
     }
 }
