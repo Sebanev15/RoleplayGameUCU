8930300dcab4efd81bee6a941ff5001342d77a7d
mohrro <maurozunin@gmail.com>
2025-09-20 23:50:33 -0500

    Añadido HealValue: int a Shield y borrado IsHealing: bool

--- src/Program/Shield.cs
+++ src/Program/Shield.cs
@@ -6,13 +6,13 @@ public class Shield: IItem
     public int Attack { get; set; }
     public int Defense { get; set; }
     public bool IsMagical { get; set; }
-    public bool IsHealing { get; set; }
+    public int HealValue { get; set; }
     public Shield(string unNombre, int attack, int defense, bool isMagical, bool isHealing)
     {
         this.Name = unNombre;
         this.Attack = attack;
         this.Defense = defense;
         this.IsMagical = isMagical;
-        this.IsHealing = isHealing;
+        this.HealValue = 0;
     }
 }
